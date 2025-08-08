859baed62f114628e9c356d833e9fe054eea6692
cvclark <a02225430@aggies.usu.edu>
2025-08-08 15:44:55 -0700

    Removed some commented out code

--- src/main/kotlin/PasswordDetector.kt
+++ src/main/kotlin/PasswordDetector.kt
@@ -2,11 +2,6 @@ class PasswordDetector: Detector {
     override var startState: State = NoCapitalNoSpecialCharState()
 
     override fun isValid(string: String): Boolean {
-//        var state = startState
-//        for (letter in string) {
-//            state = state.consumeLetter(letter.toString())
-//        }
-//        return (state is ValidState) and (string.length > 7)
         return super.isValid(string) and (string.length > 7)
     }
 }
